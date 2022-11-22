3956f18990d4dde7e3f55e4bd52da9fec0397b85
Sam Sarsten <sam.sarsten@gmail.com>
2022-11-21 18:50:50 -0800

    Initial commit

---
+++ rust-crash-course/target/debug/deps/rust_crash_course-59ca5d877003f17d.d
@@ -0,0 +1,5 @@
+/Users/sam/Documents/Projects/repos/starting-rust/rust-crash-course/target/debug/deps/rust_crash_course-59ca5d877003f17d.rmeta: src/main.rs
+
+/Users/sam/Documents/Projects/repos/starting-rust/rust-crash-course/target/debug/deps/rust_crash_course-59ca5d877003f17d.d: src/main.rs
+
+src/main.rs:
---
+++ rust-crash-course/target/debug/deps/rust_crash_course-cd1751face0ed082.d
@@ -0,0 +1,5 @@
+/Users/sam/Documents/Projects/repos/starting-rust/rust-crash-course/target/debug/deps/rust_crash_course-cd1751face0ed082.rmeta: src/main.rs
+
+/Users/sam/Documents/Projects/repos/starting-rust/rust-crash-course/target/debug/deps/rust_crash_course-cd1751face0ed082.d: src/main.rs
+
+src/main.rs:
